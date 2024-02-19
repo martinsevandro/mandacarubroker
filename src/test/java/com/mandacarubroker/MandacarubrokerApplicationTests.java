bd99b6ba999a7b1667960fb774cdad0c182037bc
evandro <evandro.filho@outlook.com.br>
2024-02-19 20:49:37 -0300

    feature-test updatePrice and contextLoads

--- src/test/java/com/mandacarubroker/MandacarubrokerApplicationTests.java
+++ src/test/java/com/mandacarubroker/MandacarubrokerApplicationTests.java
@@ -1,5 +1,7 @@
 package com.mandacarubroker;
 
+import com.mandacarubroker.domain.stock.Stock;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,14 @@ class MandacarubrokerApplicationTests {
 
 	@Test
 	void contextLoads() {
+		//Arrange
+		Stock stock = new Stock("id","simbolo","cia",110);
+
+		//Act
+		stock.setPrice(120);
+
+		//Assert
+		Assertions.assertEquals(120, stock.getPrice(),"stock price updated");
 	}
 
 }
